ee990814b532b4c3194ac97089bb4484782bedfb
erikamedeiros <65049180+erikamedeiros@users.noreply.github.com>
2020-12-21 16:30:47 -0300

    Add files via upload

---
+++ affinity.py
@@ -0,0 +1,43 @@
+#Affinity Propagation para clusterização e comparação com o k-means
+
+from numpy import unique
+from numpy import where
+
+from sklearn.cluster import AffinityPropagation
+from matplotlib import pyplot
+
+import numpy as np #para manipular os vetores
+
+import pandas as pd #para abrir arquivos
+from matplotlib import pyplot as plt #para plotar os gráficos
+from sklearn.cluster import KMeans #para usar o KMeans
+from sklearn.preprocessing import MinMaxScaler #para normalizar
+from sklearn.cluster import AffinityPropagation
+
+#carregando arquivo
+arq = pd.read_csv('dataset.csv', sep=';')  
+
+#criando array do arquivo csv
+dataset = np.array(arq)
+
+#normalizando array
+#normalized = MinMaxScaler(feature_range = (50, 200)) #valor para normalizacao
+#x_norm = normalized.fit_transform(dataset) #normalizando
+
+
+#montando grupos (clusters)
+model = AffinityPropagation(damping=0.9)
+model.fit(dataset)
+
+# assign a cluster to each example
+yhat = model.predict(dataset)
+# retrieve unique clusters
+clusters = unique(yhat)
+# create scatter plot for samples from each cluster
+for cluster in clusters:
+	# get row indexes for samples with this cluster
+	row_ix = where(yhat == cluster)
+	# create scatter of these samples
+	pyplot.scatter(dataset[row_ix, 0], dataset[row_ix, 1])
+# show the plot
+pyplot.show()
